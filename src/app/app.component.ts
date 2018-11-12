915202e13623e7b8f7bfd163dc16f012ce7a8162
radiium <remip.dev@gmail.com>
2018-11-12 14:26:37 +0100

    Rework package.json scripts and add support to native node module in angular when running in electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import * as fs from 'fs';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  fs: any; // typeof fs;
+
+  constructor() {
+    // this.fs = window.require('fs');
+    console.log('Test');
+    console.log(this.fs);
+  }
 }
